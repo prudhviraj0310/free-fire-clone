855d869bda1fae35d423093fdaf2492dd947433c
prudhvi raj <prudhviraj@prudhvis-MacBook-Air.local>
2025-12-18 15:33:42 +0530

    chore: remove server code from app repo (moved to separated repo)

--- server/middleware/roleMiddleware.js
+++
@@ -1,8 +0,0 @@
-const roleMiddleware = (roles) => (req, res, next) => {
-    if (!req.user || !roles.includes(req.user.role)) {
-        return res.status(403).json({ message: "Forbidden: Insufficient Permissions" });
-    }
-    next();
-};
-
-module.exports = roleMiddleware;
